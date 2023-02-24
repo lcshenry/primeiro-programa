e36b66b1dd14b870025280053d47444bfb20733c
lcshenry <lucashenrique51513@gmail.com>
2023-02-24 13:35:40 -0300

    minha classe

--- src/anatomia/das/classes/MinhaClasse.java
+++ src/anatomia/das/classes/MinhaClasse.java
@@ -1,7 +1,11 @@
 package anatomia.das.classes;
 
 public class MinhaClasse {
+
     public static void main(String[] args) {
+        System.out.println("Primeira Aula de Anatomia das Classes!");
+        System.out.println("Sejam bem-vindos :D");
 
     }
+
 }
