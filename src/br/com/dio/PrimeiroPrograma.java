70f7af148737241d0f3bfa47117f48f5c6602d84
lcshenry <lucashenrique51513@gmail.com>
2023-02-24 10:47:50 -0300

    codigo em java

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,5 +1,20 @@
 package br.com.dio;
 
 public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        String firstName = "Lucas";
+        String lastName = "Henrique";
+        String myJob = "Programming Language / Student";
 
+        int anoAtual = 2023;
+        int anoNascimento = 1999;
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println("My name is" + " " + firstName + " " + lastName + ". "
+                + "I have" + " " + idade + " " +
+                "years old" + ", " + "my recently job is" + " "
+                + myJob);
+
+
+    }
 }
